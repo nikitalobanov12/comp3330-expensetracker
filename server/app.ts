1bba417c06a3242e147b93012ded7003201f426a
nikitalobanov12 <nikitaalobanovv@gmail.com>
2025-09-12 13:04:23 -0700

    Lab 2, Lab 2: implemeneted more routes, Zod validation, & Custom timing middleware

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,22 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { expensesRoute } from "./routes/expenses";
 
 export const app = new Hono();
 
 app.use("*", logger());
 
+//hono middleware
+app.use("*", async (c, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  // This is a response header that shows the response time of the request
+  c.header("X-Response-Time", `${ms}ms`);
+});
+
 app.get("/", (c) => c.json({ message: "OK" }));
 app.get("/health", (c) => c.json({ status: "healthy" }));
-app.get("/api/test", (c) => c.json({ message: "test" }));
+//Mount API Routes
+
+app.route("api/expenses", expensesRoute);
